64550533148bcb5b89586ac3f27df7fdbce4f0e7
Gerard Mangues <gerardcoding@gmail.com>
2021-06-13 13:04:21 +0200

    fix navigaition

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,7 +7,7 @@ import * as styles from './navigationStyles.module.css';
 export default function Navigation({ step }) {
   return (
     <div className={styles.container}>
-      <Link to={step === 1 ? '/' : `step${step}`} type="button" className={styles.backLink}>
+      <Link to={step === 1 ? '/' : `step${step - 1}`} type="button" className={styles.backLink}>
         <IoIosArrowBack className={styles.position__absolute} />
         Volver
       </Link>
